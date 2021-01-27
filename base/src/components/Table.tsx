eba4bb22c90330895a2c0a120f4cfd833c047a43
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2021-01-27 09:09:35 +0300

    (+) base.

---
+++ base/src/components/Table.tsx
@@ -0,0 +1,147 @@
+import React from "react";
+import styled from "styled-components/macro";
+import { useTable, useBlockLayout } from "react-table";
+
+
+const StyledTable = styled.div`
+  & {
+    height: 50%;
+  }
+
+  #content {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+
+/* A bit more styling to make it look better */
+
+  .table-wrapper {
+    background: CadetBlue;
+    overflow: scroll;
+    flex-grow: 1;
+  }
+
+  .table-wrapper .table {
+    width: 100%;
+  }
+
+  .table-wrapper .th {
+    background: #DDD;
+  }
+
+  .table-wrapper .td {
+    background: CadetBlue;
+  }
+
+  .table-wrapper .td, .th {
+    padding: 4px 4px 4px 4px;
+    text-align: center;  
+    border: 1px solid black;
+    border-spacing: 0;
+  }
+
+  .table-wrapper .td div, .th div {  
+    height: 100%;
+  }
+`;
+
+
+export interface ColumnInterface {
+  id: string,
+  Header: string,
+  accessor: Function,
+}
+
+function ReactTable({ columns, data }: any) {
+  const defaultColumn = React.useMemo(
+    () => ({
+      minWidth: 30,
+      width: 150,
+      maxWidth: 400,
+    }),
+    [],
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+
+    state,
+  }: any = useTable(
+    {
+      columns,
+      data,
+      defaultColumn,
+    },
+    useBlockLayout,
+  );
+
+  return (
+    <>
+      <div>
+        <div className="table" {...getTableProps()}>
+
+          {/* ----- Header ----- */}
+          <div className="thead">
+            {headerGroups.map((headerGroup: any, i_headerGroup: number) => (
+              <div className="tr" {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column: any, i_column: number) => {
+                  return (
+                    <div className="th" {...column.getHeaderProps()}>
+                      {column.render("Header")}
+                    </div>  // </th>
+                  );
+                })}
+              </div>  // </tr>
+            ))}
+          </div>
+          {/* thead */}
+
+          {/* ----- Body ----- */}
+          <div className="tbody" {...getTableBodyProps()}>
+            {rows.map((row: any, i_row: number) => {
+              prepareRow(row);
+              return (
+                <div className="tr" {...row.getRowProps()}>
+                  {row.cells.map((cell: any, i_cell: number) => {
+                    return (
+                      <div className="td" {...cell.getCellProps()}>
+                        {cell.render("Cell")}
+                      </div>  // td
+                    );
+                  })}
+                </div>  // tr
+              );
+            })}
+          </div>
+          {/* tbody */}
+
+        </div>
+        {/* table */}
+      </div>
+    </>
+  );
+}
+
+
+function Table(props: { columns: Array<ColumnInterface>, data: Array<Object> }) {
+  return (
+    <StyledTable>
+      <div id="content">
+        <div>Text before</div>
+        <div className="table-wrapper">
+          <ReactTable columns={props.columns} data={props.data}/>
+        </div>
+        <div>Text after</div>
+      </div>
+    </StyledTable>
+  );
+}
+
+
+export default Table;
