41ca561107fb36e6818700af7127a450428e913a
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2021-01-30 19:34:12 +0300

    (decor)

--- sort_by_column/src/components/Table/Table.tsx
+++ sort_by_column/src/components/Table/Table.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components/macro";
-import { useTable, useBlockLayout, useSortBy, HeaderPropGetter } from "react-table";
+import { useTable, useBlockLayout, useSortBy } from "react-table";
 import { TableColumnSorter } from "./features/SortByColumn";
 
 
