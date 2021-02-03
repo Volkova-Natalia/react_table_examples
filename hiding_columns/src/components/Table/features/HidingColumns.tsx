63c43e23f2ecdc0566cee0f434eed36473f831fc
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2021-02-03 22:37:33 +0300

    (refactor) separated common part (hiding_columns). (refactor) deleted unused code (hiding_columns).

--- hiding_columns/src/components/Table/features/HidingColumns.tsx
+++ hiding_columns/src/components/Table/features/HidingColumns.tsx
@@ -14,14 +14,6 @@ const TableHidingColumnsSelectorStyled = styled.div`
   padding: 16px 16px 16px 16px;
 `;
 
-// const AllOnStyled = styled.button`
-//
-// `;
-//
-// const AllOffStyled = styled.button`
-//
-// `;
-
 // --------------------------------------------------
 
 function getMayHiddenColumns(allColumns: Array<any>) {
@@ -34,61 +26,51 @@ function getMayHiddenColumns(allColumns: Array<any>) {
   return columns;
 }
 
-// --------------------------------------------------
 
-// let checkboxes = Array.from(document.getElementsByName("table_visible_column_checkbox"));
-// let select_deselect: any = document.getElementById("table_visible_column_select_deselect_all_checkboxes");
+function toggle(column: any, mayHiddenColumns: Array<any>, checked: boolean, countChecked: number, setStateCountChecked: Function) {
+  const newCountChecked = (checked) ? (countChecked + 1) : (countChecked - 1);
+  setStateCountChecked(newCountChecked);
 
-// --------------------------------------------------
+  let select_deselect: any = document.getElementById("table_visible_column_select_deselect_all_checkboxes");
+  if (newCountChecked === 0) {
+    select_deselect.checked = false;
+    select_deselect.indeterminate = false;
+  } else if (newCountChecked === mayHiddenColumns.length) {
+    select_deselect.checked = true;
+    select_deselect.indeterminate = false;
+  } else {
+    select_deselect.checked = false;
+    select_deselect.indeterminate = true;
+  }
 
-function toggle(on_off: "on" | "off", mayHiddenColumns: Array<any>, setStateCountChecked: Function) {
-  let checkboxes = Array.from(document.getElementsByName("table_visible_column_checkbox"));
-
-  let checked = (on_off === "on") ? true : false;
-  checkboxes.map((checkbox: any, i_checkbox: number) => {
-
-    checkbox.checked = checked;
-    // if (on_off === "on") {
-    //   checkbox.setAttribute("checked", "checked");
-    // }
-    // else {
-    //   checkbox.removeAttribute("checked");
-    // }
-  });
-
-  setStateCountChecked(() => (on_off === "on") ? mayHiddenColumns.length : 0);
-
-  let hidden = (on_off === "off") ? true : false;
-  mayHiddenColumns.map((column: any, i_column: number) => {
-    column.toggleHidden(hidden);
-  });
-
+  column.toggleHidden(!checked);
 }
 
-// function AllOn(props: { allColumns: any }) {
-//   return (
-//     <AllOnStyled id="AllOn" onClick={() => toggle("on", props.allColumns)}>
-//       ON all
-//     </AllOnStyled>
-//   );
-// }
-//
-// function AllOff(props: { allColumns: any }) {
-//   return (
-//     <AllOnStyled id="AllOff" onClick={() => toggle("off", props.allColumns)}>
-//       OFF all
-//     </AllOnStyled>
-//   );
-// }
+function toggleAll(checked: boolean, mayHiddenColumns: Array<any>, setStateCountChecked: Function) {
+  let checkboxes = Array.from(document.getElementsByName("table_visible_column_checkbox"));
+  checkboxes.map((checkbox: any, i_checkbox: number) => {
+    checkbox.checked = checked;
+  });
+
+  setStateCountChecked(() => checked ? mayHiddenColumns.length : 0);
+
+  mayHiddenColumns.map((column: any, i_column: number) => {
+    column.toggleHidden(!checked);
+  });
+}
+
+// --------------------------------------------------
+
+
+// --------------------------------------------------
 
 function SelectDeselectAll(props: { mayHiddenColumns: Array<any>, setStateCountChecked: Function }) {
   return (
     <label>
       <input type="checkbox" id="table_visible_column_select_deselect_all_checkboxes" defaultChecked={true}
              onChange={(e) =>
-               toggle(e.target.checked ? "on" : "off", props.mayHiddenColumns, props.setStateCountChecked)
+               toggleAll(e.target.checked, props.mayHiddenColumns, props.setStateCountChecked)
              }/>{" "}
-      {/*/>{" "}*/}
       Select / Deselect all
     </label>
   );
@@ -104,39 +86,15 @@ export function TableHidingColumnsSelector(props: { allColumns: Array<any> }) {
   const [countChecked, setCountChecked] = useState(Number(mayHiddenColumns.length));
   return (
     <TableHidingColumnsSelectorStyled>
-      {/*<div>*/}
-      {/*  <AllOn allColumns={props.allColumns}/>*/}
-      {/*  <AllOff allColumns={props.allColumns}/>*/}
-      {/*</div>*/}
-
       <SelectDeselectAll mayHiddenColumns={mayHiddenColumns} setStateCountChecked={setCountChecked}/>
-
       {
         mayHiddenColumns.map((column: any, i_column: number) => (
           <div key={column.id} style={{ "marginLeft": "16px" }}>
             <label>
-              {/*<input type="checkbox" name="table_visible_column_checkbox" {...column.getToggleHiddenProps()}*/}
               <input type="checkbox" name="table_visible_column_checkbox" defaultChecked={true}
-                     onChange={(e) => {
-                       const newCountChecked = (e.target.checked) ? (countChecked + 1) : (countChecked - 1);
-                       setCountChecked(newCountChecked);
-                       let select_deselect: any = document.getElementById("table_visible_column_select_deselect_all_checkboxes");
-                       if (newCountChecked === 0) {
-                         // select_deselect.removeAttribute("checked");
-                         select_deselect.checked = false;
-                         select_deselect.indeterminate = false;
-                       } else if (newCountChecked === mayHiddenColumns.length) {
-                         select_deselect.checked = true;
-                         select_deselect.indeterminate = false;
-                       } else {
-                         select_deselect.checked = false;
-                         select_deselect.indeterminate = true;
-                       }
-                       // select_deselect.indeterminate = true;
-                       // select_deselect.checked = false;
-                       // select_deselect.setAttribute("indeterminate");
-                       column.toggleHidden(!e.target.checked);
-                     }}/>{" "}
+                     onChange={(e) =>
+                       toggle(column, mayHiddenColumns, e.target.checked, countChecked, setCountChecked)
+                     }/>{" "}
               {(column.Header) ? column.Header : column.id}
             </label>
           </div>
