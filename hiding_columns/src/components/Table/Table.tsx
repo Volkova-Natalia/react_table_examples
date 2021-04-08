10fc64b887612a959a5263ee8c7f7bc80d8899c9
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2021-04-08 19:01:58 +0300

    (refactor) <div className="table"/"thead"/"tbody"/"tr"/"td"/"th" > -> appropriate components (hiding_columns).

--- hiding_columns/src/components/Table/Table.tsx
+++ hiding_columns/src/components/Table/Table.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components/macro";
 import { useTable, useBlockLayout } from "react-table";
 import { TableHidingColumnsSelector } from "./features/HidingColumns";
+import { TABLE, THEAD, TBODY, TR, TD, TH } from "./Components";
 
 
 const StyledTable = styled.div`
@@ -99,45 +100,42 @@ function ReactTable({ columns, data }: any) {
       <div>
         <TableHidingColumnsSelector allColumns={allColumns}/>
 
-        <div className="table" {...getTableProps()}>
+        <TABLE {...getTableProps()}>
 
           {/* ----- Header ----- */}
-          <div className="thead">
+          <THEAD>
             {headerGroups.map((headerGroup: any, i_headerGroup: number) => (
-              <div className="tr" {...headerGroup.getHeaderGroupProps()}>
+              <TR {...headerGroup.getHeaderGroupProps()}>
                 {headerGroup.headers.map((column: any, i_column: number) => {
                   return (
-                    <div className="th" {...column.getHeaderProps()}>
+                    <TH {...column.getHeaderProps()}>
                       {column.render("Header")}
-                    </div>  // </th>
+                    </TH>
                   );
                 })}
-              </div>  // </tr>
+              </TR>
             ))}
-          </div>
-          {/* thead */}
+          </THEAD>
 
           {/* ----- Body ----- */}
-          <div className="tbody" {...getTableBodyProps()}>
+          <TBODY {...getTableBodyProps()}>
             {rows.map((row: any, i_row: number) => {
               prepareRow(row);
               return (
-                <div className="tr" {...row.getRowProps()}>
+                <TR {...row.getRowProps()}>
                   {row.cells.map((cell: any, i_cell: number) => {
                     return (
-                      <div className="td" {...cell.getCellProps()}>
+                      <TD {...cell.getCellProps()}>
                         {cell.render("Cell")}
-                      </div>  // td
+                      </TD>
                     );
                   })}
-                </div>  // tr
+                </TR>
               );
             })}
-          </div>
-          {/* tbody */}
+          </TBODY>
 
-        </div>
-        {/* table */}
+        </TABLE>
       </div>
     </>
   );
